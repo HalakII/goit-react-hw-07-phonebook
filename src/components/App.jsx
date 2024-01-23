dea9bd54f0507260071f2b6582d76ed392ebb69e
HalakII <1017imiia@gmail.com>
2024-01-23 19:33:07 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer } from 'react-notifications';
+import { ContactsList } from './ContactsList/ContactsList';
+import { SectionTitle } from './SectionTitle/SectionTitle';
+import { SectionSubtitle } from './SectionSubtitle/SectionSubtitle';
+import { ContactForm } from './Form/ContactForm';
+import { ContactsFilter } from './Filter/Filter';
+import css from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      <NotificationContainer />
+      <SectionTitle title="Phonebook" />
+      <ContactForm />
+      <SectionSubtitle subtitle="Contacts" />
+      <ContactsFilter />
+      <ContactsList />
     </div>
   );
 };
---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,37 @@
+import { ContactsListItem } from 'components/ContactsListItem/ContactsListItem';
+import css from './ContactsList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { deleteContact } from '../../redux/contactsSlice';
+import { getContacts, getFilter } from '../../redux/selectors';
+
+export const ContactsList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  console.log(filter);
+
+  const getfilterContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const handleDelete = id => dispatch(deleteContact(id));
+
+  const filteredContacts = getfilterContacts();
+  return (
+    <ul className={css.contactList}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <ContactsListItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDeleteContact={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+};
